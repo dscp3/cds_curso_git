0cb0e3b686e71229bf5a82d8fcf26f7cd63864e5
Diego Coimbra <diego-santana@msn.com>
2023-08-30 21:27:49 -0300

    new print message function added

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 def gather_data():
     n1 = input('first value: ')
     n2 = input('second value: ')
-    op = input('operation: ')
 
-    return n1,n2,op
+    return n1,n2
+
+def print_message(n1,n2):
+    print(f'os valores {n1} e {n2} somados dão {n1+n2}')
 
 def main():
-    n1,n2,op = gather_data()
-    print(eval(n1+op+n2))
+    n1,n2 = gather_data()
+    print_message(n1,n2)
     return None
 
 if __name__ == '__main__':
